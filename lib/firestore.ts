07b64287b1b1eff6639571a0a9e61c53dbe545aa
Guido <[guidogauna9@gmail.com]>
2023-02-21 23:15:25 -0300

    probando

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -2,14 +2,9 @@ import admin from "firebase-admin"
 
 let serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE);
 
-if(admin.app.length == 0){
+if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-      });
-}
-
-
-const fireStore = admin.firestore()
-export {
-    fireStore
-}
+      credential: admin.credential.cert(serviceAccount),
+    });
+  }
+  export const firestore = admin.firestore();
